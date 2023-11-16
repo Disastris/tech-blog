38448a927d8fe8efb5d168bfe9d559ba9d38444d
Disastris <chavez.garcia.santi@gmail.com>
2023-11-16 16:27:01 -0600

    Was able to get the login screen to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,44 +26,44 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/blog/:id", async (req, res) => {
-//   try {
-//     const blogData = await Blog.findbyPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["id"],
-//         },
-//       ],
-//     });
-//     const blog = blogData.get({ plain: true });
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findbyPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["id"],
+        },
+      ],
+    });
+    const blog = blogData.get({ plain: true });
 
-//     res.render("blog", {
-//       ...blog,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render("blog", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get("/blog", withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ["password"] },
-//       include: [{ model: Blog }],
-//     });
+router.get("/blog", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
+    });
 
-//     const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-//     res.render("profile", {
-//       ...user,
-//       logged_in: true,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
